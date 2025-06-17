cae29ea530517be467bd5a0c23d17c4c845a9d95
mafreud <freud427@gmail.com>
2025-06-17 16:50:19 +0900

    Add OpenciWorker formula and installation instructions to README

---
+++ Formula/openci-worker.rb
@@ -0,0 +1,29 @@
+class OpenciWorker < Formula
+  desc "OpenCI Worker CLI"
+  homepage "https://github.com/openci-inc/openci"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/openci-inc/openci/releases/download/v0.1.0/openci-worker-0.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "YOUR_SHA256_HERE"
+    else
+      url "https://github.com/openci-inc/openci/releases/download/v0.1.0/openci-worker-0.1.0-x86_64-apple-darwin.tar.gz"
+      sha256 "YOUR_SHA256_HERE"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/openci-inc/openci/releases/download/v0.1.0/openci-worker-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "YOUR_SHA256_HERE"
+  end
+
+  def install
+    bin.install "openci-worker"
+  end
+
+  test do
+    system "#{bin}/openci-worker", "--version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# OpenCI Worker
+
+## Install
+
+```bash
+brew tap openci-inc/tap
+brew install openci-worker
+```
