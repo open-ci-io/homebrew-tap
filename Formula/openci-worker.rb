5f5406e76f0b374ae1cea75c1fc7614121031dc0
mafreud <freud427@gmail.com>
2025-06-17 16:59:06 +0900

    Update homepage and download URLs for OpenciWorker to reflect new repository location

--- Formula/openci-worker.rb
+++ Formula/openci-worker.rb
@@ -1,21 +1,21 @@
 class OpenciWorker < Formula
   desc "OpenCI Worker CLI"
-  homepage "https://github.com/openci-inc/openci"
+  homepage "https://github.com/open-ci-io/openci"
   version "0.1.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/openci-inc/openci/releases/download/v0.1.0/openci-worker-0.1.0-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/open-ci-io/openci/releases/download/v0.1.0/openci-worker-0.1.0-aarch64-apple-darwin.tar.gz"
       sha256 "78130d5133f49caaef84e84206940cdc37774d9b598e809b7b685444204d51f4"
     else
-      url "https://github.com/openci-inc/openci/releases/download/v0.1.0/openci-worker-0.1.0-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/open-ci-io/openci/releases/download/v0.1.0/openci-worker-0.1.0-x86_64-apple-darwin.tar.gz"
       sha256 "YOUR_SHA256_HERE"
     end
   end
 
   on_linux do
-    url "https://github.com/openci-inc/openci/releases/download/v0.1.0/openci-worker-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
+    url "https://github.com/open-ci-io/openci/releases/download/v0.1.0/openci-worker-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "YOUR_SHA256_HERE"
   end
 
