f08622f48f8734efd1b6001cb2683fc98f6bc027
mafreud <freud427@gmail.com>
2026-02-25 13:16:12 +0900

    fix: update sha256 for v0.4.0 (removed dotenv)

--- Formula/openci-worker.rb
+++ Formula/openci-worker.rb
@@ -7,7 +7,7 @@ class OpenciWorker < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/open-ci-io/openci/releases/download/openci-worker-v0.4.0/openci-worker-0.4.0-aarch64-apple-darwin.tar.gz"
-      sha256 "5e0cd6374aad14ed0aa2f511dfe8ece75611f643a47c826588067e10aafac0a5"
+      sha256 "6a2f35597fa647cb85491d2ef02664dfb25a05473164f0810a505116356e7fb2"
     end
   end
 
