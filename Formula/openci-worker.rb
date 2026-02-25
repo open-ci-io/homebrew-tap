450aa7eae557eae6cfc01b05a5ec9d2ce87e483b
Masahiro Aoki <m.aoki@open-ci.io>
2026-02-25 10:28:52 +0900

    feat: bump openci-worker to v0.3.0 with auto-update

--- Formula/openci-worker.rb
+++ Formula/openci-worker.rb
@@ -1,13 +1,13 @@
 class OpenciWorker < Formula
   desc "OpenCI Worker CLI"
   homepage "https://github.com/open-ci-io/openci"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/open-ci-io/openci/releases/download/openci-worker-v0.2.0/openci-worker-0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "7eeb1a848a12e9c84a10bfba6be497511090ca9a84b48226c132038576ed612b"
+      url "https://github.com/open-ci-io/openci/releases/download/openci-worker-v0.3.0/openci-worker-0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "70fd4ea57c028905a4907c42a1e43ad81920b90481c09c711ed3deab9d6b0155"
     end
   end
 
