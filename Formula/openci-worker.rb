a5de95ae43990877ddffb33c4a7a9e4a48e0fe10
Masahiro Aoki <m.aoki@open-ci.io>
2026-02-25 10:21:42 +0900

    feat: bump openci-worker to v0.2.0 with machine info display

--- Formula/openci-worker.rb
+++ Formula/openci-worker.rb
@@ -1,13 +1,13 @@
 class OpenciWorker < Formula
   desc "OpenCI Worker CLI"
   homepage "https://github.com/open-ci-io/openci"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/open-ci-io/openci/releases/download/openci-worker-v0.1.0/openci-worker-0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "481286355d9a94aab7b99f337324494915a1d79325d912466a5a3803d4931b00"
+      url "https://github.com/open-ci-io/openci/releases/download/openci-worker-v0.2.0/openci-worker-0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "7eeb1a848a12e9c84a10bfba6be497511090ca9a84b48226c132038576ed612b"
     end
   end
 
