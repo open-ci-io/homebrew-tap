d7eae502ad51afc7164c51b8441aa0f848426ea2
mafreud <freud427@gmail.com>
2026-02-26 22:14:06 +0900

    Update openci-worker 0.4.6 sha256

--- Formula/openci-worker.rb
+++ Formula/openci-worker.rb
@@ -7,7 +7,7 @@ class OpenciWorker < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/open-ci-io/openci/releases/download/openci-worker-v0.4.6/openci-worker-0.4.6-aarch64-apple-darwin.tar.gz"
-      sha256 "9a5b9448c15f6d674314b0ce0eb68839b63c0cd7296486a25ffaa6fa9fd3c7b9"
+      sha256 "bd312e5d245ed95bf3dad382c61cb96e1dc597ae4aeeadaa48590be08e236681"
     end
   end
 
