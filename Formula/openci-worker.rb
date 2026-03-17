20df2f1e2d856ff4cd0383258c9636eea100c6d9
mafreud <freud427@gmail.com>
2026-03-17 12:46:39 +0900

    fix: correct SHA256 hash for openci-worker v0.7.13 tar.gz

--- Formula/openci-worker.rb
+++ Formula/openci-worker.rb
@@ -7,7 +7,7 @@ class OpenciWorker < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/open-ci-io/openci/releases/download/v0.7.13/openci-worker-v0.7.13-darwin-arm64.tar.gz"
-      sha256 "7132c9e22afc59d335618dd98ed64cc89d3fdecd66cd88e9272f50f55c833474"
+      sha256 "81b9ea8205b438434026948bf4967fc23966f4c4f37a7321176650209cf36f6e"
     end
   end
 
