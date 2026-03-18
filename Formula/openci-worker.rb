0cfe689bc431c4a7b600f4f2b9333d2978154c5e
mafreud <freud427@gmail.com>
2026-03-18 10:28:48 +0900

    bump openci-worker to v0.7.16

--- Formula/openci-worker.rb
+++ Formula/openci-worker.rb
@@ -1,13 +1,13 @@
 class OpenciWorker < Formula
   desc "OpenCI Worker CLI"
   homepage "https://github.com/open-ci-io/openci"
-  version "0.7.15"
+  version "0.7.16"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/open-ci-io/openci/releases/download/v0.7.15/openci-worker-v0.7.15-darwin-arm64.tar.gz"
-      sha256 "56d65192603ba81fc303c58a371c6261a55449eb1f2a6bff8e07dcd015e60cf3"
+      url "https://github.com/open-ci-io/openci/releases/download/v0.7.16/openci-worker-v0.7.16-darwin-arm64.tar.gz"
+      sha256 "eaa63764db7026cbc38e6c0b04be190aef9acec799ef508c07a5e8cf545db7f0"
     end
   end
 
