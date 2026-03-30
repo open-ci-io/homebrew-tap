b097b004aba701faeb67fa24a8c7de719738f0eb
Masahiro Aoki <m.aoki@open-ci.io>
2026-03-30 11:29:36 +0200

    Update openci-worker to v0.7.20

--- Formula/openci-worker.rb
+++ Formula/openci-worker.rb
@@ -1,12 +1,12 @@
 class OpenciWorker < Formula
   desc "OpenCI Worker CLI"
   homepage "https://github.com/open-ci-io/openci"
-  version "0.7.19"
+  version "0.7.20"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/open-ci-io/openci/releases/download/v0.7.19/openci-worker-v0.7.19-darwin-arm64.tar.gz"
-      sha256 "842b862c4634a35cbe05a2f4001bec0bb1ad68796e5ad385ea62ccb0f8731f4f"
+      url "https://github.com/open-ci-io/openci/releases/download/v0.7.20/openci-worker-v0.7.20-darwin-arm64.tar.gz"
+      sha256 "7a1a786a2fc9c01871395e48520156b8a24d5065fe654134fd80150bd025bfac"
     end
   end
 
