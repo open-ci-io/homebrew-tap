8bdbdb21c69e010782d6b1bd8e7fa4db8dfc0df4
Masahiro Aoki <m.aoki@open-ci.io>
2026-04-08 02:16:34 +0200

    chore: update openci-worker to v0.8.4

--- Formula/openci-worker.rb
+++ Formula/openci-worker.rb
@@ -1,12 +1,12 @@
 class OpenciWorker < Formula
   desc "OpenCI Worker CLI"
   homepage "https://github.com/open-ci-io/openci"
-  version "0.8.3"
+  version "0.8.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/open-ci-io/openci/releases/download/v0.8.3/openci-worker-v0.8.3-darwin-arm64.tar.gz"
-      sha256 "1313a724f311396ce8e3b21b8b7f590c9f90b5194cb267e78f3007ce31ae003c"
+      url "https://github.com/open-ci-io/openci/releases/download/v0.8.4/openci-worker-v0.8.4-darwin-arm64.tar.gz"
+      sha256 "81fe40a5b9405c5a9b8fde3950f689ab38c54087935293b979d21eecf77be048"
     end
   end
 
