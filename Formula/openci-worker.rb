6ee6067a53e965c717894378c8739f7608f3a693
Masahiro Aoki <m.aoki@open-ci.io>
2026-04-18 18:24:59 +0900

    chore: bump openci-worker to v0.9.3

--- Formula/openci-worker.rb
+++ Formula/openci-worker.rb
@@ -1,12 +1,12 @@
 class OpenciWorker < Formula
   desc "OpenCI Worker CLI"
   homepage "https://github.com/open-ci-io/openci"
-  version "0.9.2"
+  version "0.9.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/open-ci-io/openci/releases/download/v0.9.2/openci-worker-v0.9.2-darwin-arm64.tar.gz"
-      sha256 "89d9599662560dd1a105f7fe2cbaef2be1fcd97070f1338e472e6980e663e1b3"
+      url "https://github.com/open-ci-io/openci/releases/download/v0.9.3/openci-worker-v0.9.3-darwin-arm64.tar.gz"
+      sha256 "ad15860cb7a78f09fd2b6b0848e0c87fd71725623c460392e839f8f8c1e403e4"
     end
   end
 
